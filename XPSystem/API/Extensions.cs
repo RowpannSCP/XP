0ebd49fddd6341bae2094ad1bae9e9a1f9f503b7
moddedmcplayer <moddedmcplayer@gmail.com>
2022-06-16 20:55:16 +0200

    forgor

--- XPSystem/API/Extensions.cs
+++ XPSystem/API/Extensions.cs
@@ -1,4 +1,6 @@
 ﻿using System.Collections.Generic;
+using AdvancedHints;
+using AdvancedHints.Enums;
 using Exiled.API.Features;
 using XPSystem.API.Serialization;
 
@@ -31,6 +33,24 @@ namespace XPSystem.API
         public static void AddXP(this PlayerLog log, int amount)
         {
             log.XP += amount;
+            Player ply = Player.Get(log.ID);
+            int lvlsGained = log.XP / Main.Instance.Config.XPPerLevel;
+            if (lvlsGained > 0)
+            {
+                log.LVL += lvlsGained;
+                log.XP -= lvlsGained * Main.Instance.Config.XPPerLevel;
+                if (Main.Instance.Config.ShowAddedLVL && ply != null)
+                {
+                    ply.ShowManagedHint(Main.Instance.Config.AddedLVLHint
+                        .Replace("%level%", log.LVL.ToString()), 3f, true, DisplayLocation.Top);
+                }
+
+                ply.RankName = "";
+            }
+            else if (Main.Instance.Config.ShowAddedXP && ply != null)
+            {
+                ply.ShowManagedHint($"+ <color=green>{amount}</color> XP", 3f, true, DisplayLocation.Top);
+            }
             log.UpdateLog();
         }
     }
